9c0fbe26d293489280e4eb508ed7cbe65416e853
zaremagumerova <zaremagumerova@mail.ru>
2025-05-17 23:50:16 +0500

    Твое сообщение

---
+++ Practica1.py
@@ -0,0 +1,22 @@
+my_heigh = 157
+print(my_heigh)
+my_name = "Зарема"
+my_name = "Зарема Валеева"
+print(my_name)
+pet_name = input("Как зовут вашего питомца? ")
+print("Ваш любимчик - " + pet_name)
+def print_python():
+    print("Учу Python!")
+print_python()
+def print_letter(let):
+    print(let, end='')
+
+print_letter('C')
+print_letter('т')
+print_letter('у')
+print_letter('д')
+print_letter('е')
+print_letter('н')
+print_letter('т')
+pet_name = input("Как зовут вашего питомца? ")
+print("Ваш любимчик - " + pet_name)
