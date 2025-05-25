b7d399dcc68a0eb8cbd46a416481b8b6496d595d
zaremagumerova <zaremagumerova@mail.ru>
2025-05-26 02:16:46 +0500

    задание 4

--- 03_lesson/lesson_3_task_2.py
+++ 03_lesson/lesson_3_task_2.py
@@ -4,7 +4,8 @@ catalog = [
    Smartphone(brand="Samsung",model="А-31",number="+79174586444"),
    Smartphone(brand="Realme",model="R-11",number="+79174444444"),
    Smartphone(brand="Sony",model="S-44",number="+79154586433"),
-   Smartphone(brand="Sony",model="Pro-12",number="+79051745864") 
+   Smartphone(brand="Nokia",model="Pro-12",number="+79051745864"),
+   Smartphone(brand="Huawei",model="Pro-10",number="+79051732121") 
 ]  
   
 for smartphone in catalog:
